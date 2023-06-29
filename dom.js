6b22a2b9f4d4d0882ae075966b27c33531639847
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-06-29 05:15:30 +0500

    textContent and innerHTML

--- dom.js
+++ dom.js
@@ -1,19 +1,35 @@
-const listItem = document.getElementsByTagName('li')
-console.log(listItem);
+// const listItem = document.getElementsByTagName('li')
+// console.log(listItem);
 
 
-//id bo'yicha olish
-const clickBtn = document.getElementById('click-btn')
-console.log(clickBtn);
+// //id bo'yicha olish
+// const clickBtn = document.getElementById('click-btn')
+// console.log(clickBtn);
 
 
-// queryselectorAll array [nodeList] qaytaradi va uni forEach qilsa boladi lekin u to'la qonli array emas
-const litem = document.querySelectorAll('.list-item')
+// // queryselectorAll array [nodeList] qaytaradi va uni forEach qilsa boladi lekin u to'la qonli array emas
+// const litem = document.querySelectorAll('.list-item')
+// listItem.forEach((item) => {
+//     console.log(litem);
+// })
+
+
+// //id bo'yicha olish
+// const title = document.querySelector('#title')
+// console.log(title);
+
+const title = document.getElementById('title')
+const listItem = document.querySelectorAll('.list-item')
+const peopleList = document.querySelector('.people-list')
+const names = ['Khayot', 'Doston', 'Donyor']
+
+title.textContent += ' New text';
+
 listItem.forEach((item) => {
-    console.log(litem);
+    item.textContent += ' darslari'
+    console.log(item);
 })
 
-
-//id bo'yicha olish
-const title = document.querySelector('#title')
-console.log(title);
+names.forEach((name) => {
+    peopleList.innerHTML += `<li>${name}</li>`
+})
