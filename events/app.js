60a11cde583b1e4f8752aae721a19a983a568d88
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-07-02 05:23:17 +0500

    create and remove

--- events/app.js
+++ events/app.js
@@ -3,15 +3,30 @@
 //targer qay biri bosilganligini korsatadi
 
 const button = document.querySelector('button');
+const ul = document.querySelector('ul');
 const items = document.querySelectorAll('li');
 
+// button.addEventListener('click', () => {
+//     console.log('you are clicked me');
+// })
+
+
+
+// ul.remove()
+
 button.addEventListener('click', () => {
-    console.log('you are clicked me');
+    // ul.innerHTML += '<li>Somethins new text</li>'
+
+    //js da element yarartsh
+
+    const li = document.createElement('li')
+    li.textContent = 'Something new text '
+    ul.appendChild(li) //prepend (li) tepadan qowadi
 })
+
 
 items.forEach((item) => {
     item.addEventListener('click', (e) => {
-        e.target.style.textDecoration = 'line-through';
-        e.target.style.opacity = '0.8';
+        e.target.remove()
     })
 })
