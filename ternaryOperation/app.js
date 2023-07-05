ada5ba01878843764ed67d7efa010082639858ea
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-07-05 12:51:04 +0500

    ternary operation

---
+++ ternaryOperation/app.js
@@ -0,0 +1,9 @@
+const age = prompt("Yoshingizni kiriting");
+
+age < 18 ? alert("siz hali voyaga yetmagansiz") : alert("Siz voyaga yetgansiz");
+
+// age < 18
+//   ? alert(`Sizning yoshingiz armiya yoshidan ${18 - age} ga kichik`)
+//   : age < 28
+//   ? alert(`Siz armiyaga borishingiz mumkin`)
+//   : alert(`Siz armiya yoshidan ${age - 28} yosh kattasiz`);
