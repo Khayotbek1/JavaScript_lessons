ae04ad3580a914741b973de6d93050abe2d5b4fe
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-07-05 21:58:21 +0500

    destructing

---
+++ destructuring/app.js
@@ -0,0 +1,7 @@
+// destructing
+// const [apple, melon, cherry] = ["olma", "qovun", "gilos"];
+// console.log(apple, melon, cherry);
+
+// rest operatori
+const [apple, melon, ...others] = ["olma", "qovun", "gilos", "limon"];
+console.log(apple, melon, others);
