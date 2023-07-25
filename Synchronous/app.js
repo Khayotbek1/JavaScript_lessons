b5db42c515fe5e8fffa999516c6ac4dbe5871590
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-07-25 16:37:26 +0500

    Ajax

---
+++ Synchronous/app.js
@@ -0,0 +1,14 @@
+console.log("hello");
+console.log("world");
+setTimeout(() => {
+  console.log("3 sek");
+}, 3000);
+setTimeout(() => {
+  console.log("4 sek");
+}, 4000);
+setTimeout(() => {
+  console.log("2 sek");
+}, 2000);
+
+console.log(1);
+console.log(2);
